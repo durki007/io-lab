26fd47d7cd1247af4cdd4427a6f07d6a68cd261d
durki <michal@durkalec.com>
2024-01-29 20:30:23 +0100

    test base

---
+++ src/test/java/pl/pwr/io/services/PackageServiceTest.java
@@ -0,0 +1,13 @@
+package pl.pwr.io.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PackageServiceTest {
+
+    @Test
+    void isPackageValid() {
+        assertTrue(true);
+    }
+}
